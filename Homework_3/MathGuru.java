d7ecb5bf7a87e3fa3d099f563ad41bb30c4ccb8f
yborai <yborai@stevens.edu>
2017-09-21 21:36:14 -0400

    Creating folder for team members to submit method files.

---
+++ Homework_3/MathGuru.java
@@ -0,0 +1,15 @@
+/*
+Homework #3
+Group Members:
+-Yoseph Borai
+-Daotong Wang
+-Linlei Liu
+-Oscar Chen
+*/
+
+//Main class from which all other methods will be run
+class MathGuru{
+	public static void main(String[] arg){
+        Char 
+	}
+}

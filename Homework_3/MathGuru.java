1a5732a688f555be1c43405c742eeb517faadfb5
yborai <yborai@stevens.edu>
2017-09-22 13:10:27 -0400

    Started coding for method implementation.

--- Homework_3/MathGuru.java
+++ Homework_3/MathGuru.java
@@ -6,10 +6,40 @@ Group Members:
 -Linlei Liu
 -Oscar Chen
 */
+import java.util.*;
 
 //Main class from which all other methods will be run
 class MathGuru{
-	public static void main(String[] arg){
-        Char 
-	}
+    public static void main(String[] arg){
+        char choice = '';
+        char continue = 'y'
+        int n = 0;
+        Scanner in = new Scanner(System.in);
+
+        do{
+            do{
+                System.out.println("Enter the letter corresponding to your desired function
+                    ('p'=2^n|'f'=n!|'o'=Fibonacci)");
+                choice = in.nextChar();
+            }while(!(choice == 'q' || choice == 'f' || choice = 'o'));
+
+            do{
+                System.out.println("Enter a value for n(0 <= n <= 20:")
+                n = in.nextInt();
+            }while(n >= 0 && n <= 20);
+
+            if(choice == 'p'){
+
+            }
+            else if(choice == 'f'){
+
+            }
+            else{
+
+            }
+
+            System.out.println("Would you like to make another calculation(y/n)?")
+            continue = in.nextChar();
+        }while
+    }
 }

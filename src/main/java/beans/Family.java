04256942758067fa9c1a309e9161807808e14613
wdt250 <632909425@qq.com>
2017-10-18 05:57:15 -0400

    Update Family.java

--- src/main/java/beans/Family.java
+++ src/main/java/beans/Family.java
@@ -6,96 +6,75 @@ import java.util.ArrayList;
  * 
  * @author Linlei Liu 
  *         E-mail:lliu33@stevens.edu
- * modify Daotong's code
+ * 
+ * @editor Daotong Wang
  */
 
 public class Family{
-	
-	private String familiesId;
-	
+	private String familyId;
 	private String marriedDate;
-	
 	private String divorceDate;
-	
 	private String husbandId;
-	
 	private String husbandName;
-	
 	private String wifeId;
-	
 	private String wifeName;
-	
 	private ArrayList<String> children;
-
-	public String getFamiliesId() {
-		return familiesId;
+	
+	public String getFamilyId() {
+		return familyId;
 	}
-
-	public void setFamiliesId(String familiesId) {
-		this.familiesId = familiesId;
+	public void setFamilyId(String id) {
+		this.familyId = id;
 	}
-
 	public String getMarriedDate() {
 		return marriedDate;
 	}
-
-	public void setMarriedDate(String marriedDate) {
-		this.marriedDate = marriedDate;
+	public void setMarriedDate(String marrieddate) {
+		this.marriedDate = marrieddate;
 	}
-
 	public String getDivorceDate() {
 		return divorceDate;
 	}
-
-	public void setDivorceDate(String divorceDate) {
-		this.divorceDate = divorceDate;
+	public void setDivorceDate(String divorcedate) {
+		this.divorceDate = divorcedate;
 	}
-
 	public String getHusbandId() {
 		return husbandId;
 	}
-
-	public void setHusbandId(String husbandId) {
-		this.husbandId = husbandId;
+	public void setHusbandId(String husbandid) {
+		this.husbandId = husbandid;
 	}
-
 	public String getHusbandName() {
 		return husbandName;
 	}
-
-	public void setHusbandName(String husbandName) {
-		this.husbandName = husbandName;
+	public void setHusbandName(String husbandname) {
+		this.husbandName = husbandname;
 	}
-
 	public String getWifeId() {
 		return wifeId;
 	}
-
-	public void setWifeId(String wifeId) {
-		this.wifeId = wifeId;
+	public void setWifeId(String wifeid) {
+		this.wifeId = wifeid;
 	}
-
 	public String getWifeName() {
 		return wifeName;
 	}
-
-	public void setWifeName(String wifeName) {
-		this.wifeName = wifeName;
+	public void setWifeName(String wifename) {
+		this.wifeName = wifename;
 	}
-
 	public ArrayList<String> getChildren() {
 		return children;
 	}
-
-	public void setChildren(ArrayList<String> children) {
-		this.children = children;
+	public void setChilden(ArrayList<String> Children) {
+		this.children = Children;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Family [familiesId=" + familiesId + ", marriedDate=" + marriedDate + ", divorceDate=" + divorceDate
-				+ ", husbandId=" + husbandId + ", husbandName=" + husbandName + ", wifeId=" + wifeId + ", wifeName="
-				+ wifeName + ", children=" + children + "]";
+		return "Family [id=" + familyId + ", marrieddate=" + marriedDate + ", divorcedate=" + divorceDate + ", husbandid="
+				+ husbandId + ", husbandname=" + husbandName + ", wifeid=" + wifeId + ", wifename=" + wifeName
+				+ ", child=" + children + "]";
 	}
+	
 	
 }

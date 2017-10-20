87835730d225e05911b373a94039266a32f70633
wdt250 <632909425@qq.com>
2017-10-19 20:04:51 -0400

    just a test

--- src/main/java/userstrories/daotong/DaotongStories.java
+++ src/main/java/userstrories/daotong/DaotongStories.java
@@ -8,17 +8,18 @@ import main.java.userstrories.linlei.Us05;
 import main.java.userstrories.linlei.Us06;
 
 /**
-* @author 
+* @author
 *         E-mail:
-* @date Oct 19, 2017 
+* @date Oct 19, 2017
 *
-* @version 
+* @version
 */
 public class DaotongStories {
-	
+
 	public static void check(ArrayList<Individual> individuals, ArrayList<Family> families) {
 		Us07.DeathBeforeMarriage(individuals, families);
 		Us08.DeathBeforeDivorce(individuals, families);
+		System.out.println("Just a test");;
 	}
-	
+
 }

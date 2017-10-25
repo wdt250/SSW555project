750e8e636195945a48299809265097c2f0045b6d
wdt250 <632909425@qq.com>
2017-10-25 13:09:10 -0400

    fix DaotongStories

--- src/main/java/userstories/daotong/DaotongStories.java
+++ src/main/java/userstories/daotong/DaotongStories.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 
 import main.java.beans.Family;
 import main.java.beans.Individual;
-<<<<<<< HEAD
 import main.java.userstories.daotong.Us07;
 import main.java.userstories.daotong.Us08;
-=======
->>>>>>> branch 'master' of https://github.com/wdt250/SSW555project
+import main.java.userstories.daotong.Us14;
 
 /**
 * @author Daotong Wang
@@ -20,18 +18,10 @@ import main.java.userstories.daotong.Us08;
 */
 public class DaotongStories {
 
-<<<<<<< HEAD
 	public static void check(ArrayList<Individual> individuals, ArrayList<Family> families) {
 		Us07.LessThan150YearsOld(individuals);
 		Us08.BirthBeforeMarriageofParents(individuals, families);
 		Us14.MultipleBirthsNoMoreThan5(individuals, families);
-=======
-	public static void check(ArrayList<Individual> individuals, ArrayList<Family> families, PrintWriter outFile) {
-		Us07.DeathBeforeMarriage(individuals, families);
-		Us08.DeathBeforeDivorce(individuals, families);
-		System.out.println("abc");
-		System.out.println("Just a test");;
->>>>>>> branch 'master' of https://github.com/wdt250/SSW555project
 	}
 
 }

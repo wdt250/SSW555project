f2c6038b7ac86a8df1588cad2f9ba309c30a6bb0
wdt250 <632909425@qq.com>
2017-11-08 20:28:08 -0500

    fix US14

--- src/main/java/userstories/daotong/DaotongStories.java
+++ src/main/java/userstories/daotong/DaotongStories.java
@@ -19,7 +19,7 @@ public class DaotongStories {
 	public static void check(ArrayList<Individual> individuals, ArrayList<Family> families, PrintWriter outFile) {
 		US07.LessThan150YearsOld(individuals, outFile);
 		US08.BirthBeforeMarriageofParents(individuals, families, outFile);
-//		US14.MultipleBirthsNoMoreThan5(individuals, families, outFile);
+		US14.MultipleBirthsNoMoreThan5(individuals, families, outFile);
 		US18.SiblingsShouldNotMarry(families, outFile);
 		US19.FirstCousinsShouldNotMarry(families, outFile);
 	}

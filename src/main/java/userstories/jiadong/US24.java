b8ea07335e85d706b2b6ac8c16cfaf3f2bfdea3c
jchen69 <jchen69@stevens.edu>
2017-11-08 22:24:29 -0500

    Create US24.java

---
+++ src/main/java/userstories/jiadong/US24.java
@@ -0,0 +1,27 @@
+package main.java.userstories.jiadong;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import main.java.beans.Family;
+
+public class US24 {
+	public static boolean uniqueFamiliesBySpouses(ArrayList<Family> families, PrintWriter outFile) {
+		Boolean flag = true;
+		for(Family fa: families) {
+			for(Family fa2: families) {
+				if(fa.getFamilyId() != fa2.getFamilyId() && fa.getHusbandName().equals(fa2.getHusbandName()) && fa.getWifeName().equals(fa2.getWifeName()) && fa.getMarriedDate().equals(fa2.getMarriedDate())) {
+					System.out.println("Anomaly: FAMILY: US24 " + fa.getFamilyId() +  ": has a same family spouse and marriaed date with another family");
+					outFile.println("Anomaly: FAMILY: US24 " + fa.getFamilyId() +  ": has a same family spouse and marriaed date with another family");
+					
+					flag = false;
+				}
+				
+			}
+		}
+		outFile.flush();
+		return flag;	
+	}
+}

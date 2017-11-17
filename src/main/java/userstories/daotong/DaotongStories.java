6cc5e103f14a3e8364124052531c6883be3c34ae
wdt250 <632909425@qq.com>
2017-11-17 00:47:26 -0500

    adjust Daotong's checker to test US18 and 19

--- src/main/java/userstories/daotong/DaotongStories.java
+++ src/main/java/userstories/daotong/DaotongStories.java
@@ -17,9 +17,9 @@ import main.java.beans.Individual;
 public class DaotongStories {
 
 	public static void check(ArrayList<Individual> individuals, ArrayList<Family> families, PrintWriter outFile) {
-		US07.LessThan150YearsOld(individuals, outFile);
-		US08.BirthBeforeMarriageofParents(individuals, families, outFile);
-		US14.MultipleBirthsNoMoreThan5(individuals, families, outFile);
+//		US07.LessThan150YearsOld(individuals, outFile);
+//		US08.BirthBeforeMarriageofParents(individuals, families, outFile);
+//		US14.MultipleBirthsNoMoreThan5(individuals, families, outFile);
 		US18.SiblingsShouldNotMarry(families, outFile);
 		US19.FirstCousinsShouldNotMarry(families, outFile);
 //		US29.ListDeceased(individuals, outFile);

70264c1fe72406284f3e1c67dd5dcfb347222ec1
yborai <yborai@stevens.edu>
2017-11-20 22:25:53 -0500

    Added user stories without tests.

---
+++ src/main/java/userstories/yoseph/US26.java
@@ -0,0 +1,46 @@
+package main.java.userstories.yoseph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.PrintWriter;
+
+import main.java.beans.*;
+
+public class US26 {
+	public static boolean CorrespondingEntries(ArrayList<Individual> individuals, ArrayList<Family> families, PrintWriter outfile) {
+		boolean flag = true;
+		HashMap<String, String> ChildFamilies = new HashMap<String, String>(individuals.size() + 1);
+		HashMap<String, String> SpouseFamilies = new HashMap<String, String>(individuals.size() + 1);
+		
+		for(Individual individual: individuals) {
+			if(!(individual.getAsChildOfFamily().equals("None")))
+				ChildFamilies.put(individual.getIndividualId(), individual.getAsChildOfFamily());
+			if(!(individual.getAsSpouseOfFamily().equals("NA")))
+				SpouseFamilies.put(individual.getIndividualId(), individual.getAsSpouseOfFamily());
+		}
+
+		for(Family family: families) {
+			for(String child: family.getChildren()) {
+				if(!ChildFamilies.get(child).equals(family.getFamilyId())) {
+					flag = false;
+					System.out.println("Error:US26:Individual:(" + child + "): Family role records not consistent!");
+					outfile.println("Error:US26:Individual:(" + child + "): Family role records not consistent!");
+				}
+			}
+			
+			if((SpouseFamilies.get(family.getWifeId()) != null) && (!SpouseFamilies.get(family.getHusbandId()).equals(family.getFamilyId()) && !family.getDivorceDate().equals("NA"))) {
+				flag = false;
+				System.out.println("Error:US26:Individual:(" + family.getHusbandId() + "): Family role records not consistent!");
+				outfile.println("Error:US26:Individual:(" + family.getHusbandId() + "): Family role records not consistent!");
+			}
+			
+			if((SpouseFamilies.get(family.getWifeId()) != null) && (!SpouseFamilies.get(family.getWifeId()).equals(family.getFamilyId()) && !family.getDivorceDate().equals("NA"))) {
+				flag = false;
+				System.out.println("Error:US26:Individual:(" + family.getWifeId() + "): Family role records not consistent!");
+				outfile.println("Error:US26:Individual:(" + family.getWifeId() + "): Family role records not consistent!");
+			}
+		}
+		
+		return flag;
+	}
+}

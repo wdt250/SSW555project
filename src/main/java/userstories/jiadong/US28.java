1c81e49dbaa35cdd33dde476138956f06b2d9302
jchen69 <jchen69@stevens.edu>
2017-11-20 22:46:49 -0500

    Update US28.java

--- src/main/java/userstories/jiadong/US28.java
+++ src/main/java/userstories/jiadong/US28.java
@@ -12,6 +12,7 @@ public class US28 {
 		String arr[];
 		
 		System.out.println("US28: Below is the list of siblings in families by decreasing age: ");
+		outFile.println("\nUS28: Below is the list of siblings in families by decreasing age: ");
 		
 		for (Iterator<Family> iterator = families.iterator(); iterator.hasNext();) {
 			Family fam = (Family)iterator.next();
@@ -26,69 +27,67 @@ public class US28 {
 				count++;
 			};
 			
-			if(fSize == 1) {
-				System.out.format("%-6s%-20s%-10s%-16s%-7s%-16s%-10s%-7s", "ID","Name","Gender","Birthday",
-						"Age","Child","Spouse","family");
-				
-				for (Iterator<Individual> iterator2 = individuals.iterator(); iterator2.hasNext();) {
-					Individual indi = (Individual) iterator2.next();
+			System.out.format("%-6s%-20s%-10s%-16s%-7s%-16s%-10s%-7s", "ID","Name","Gender","Birthday",
+					"Age","Child","Spouse","Family");
+			System.out.println();
+			
+			outFile.format("%-6s%-20s%-10s%-16s%-7s%-16s%-10s%-7s", "ID","Name","Gender","Birthday",
+					"Age","Child","Spouse","Family");
+			outFile.print("\r\n");
+			
+			for(int i = 0; i < arr.length; i++) {
+				for(int j = 0; j < arr.length - i - 1; j++) {
+					int indi1Age = 0;
+					int indi2Age = 0;
 					
-					if(arr[0].equals(indi.getIndividualId())) {
+					for (Iterator<Individual> iterator3 = individuals.iterator(); iterator3.hasNext();) {
+						Individual indi = (Individual) iterator3.next();
 						
-						System.out.format("%-6s%-20s%-10s%-16s%-7d%-16s%-10s%-7s", 	indi.getIndividualId(),
-																				indi.getName(),
-																				indi.getGender(),
-																				indi.getBirthDate(),
-																				indi.getAge(),
-																				indi.getAsChildOfFamily(),
-																				indi.getAsSpouseOfFamily(),
-																				fam.getFamilyId());
-					}
-				}
-			}else {
-				System.out.format("%-6s%-20s%-10s%-16s%-7s%-16s%-10s", "ID","Name","Gender","Birthday",
-						"Age","Child","Spouse");
-				for(int i = 0; i < arr.length; i++) {
-					for(int j = 0; j < arr.length - i - 1; j++) {
-						int indi1Age = 0;
-						int indi2Age = 0;
-						
-						for (Iterator<Individual> iterator3 = individuals.iterator(); iterator3.hasNext();) {
-							Individual indi = (Individual) iterator3.next();
-							
-							if(arr[j].equals(indi.getIndividualId())) {
-								indi1Age = indi.getAge();
-							}
-							
-							if(arr[j+1].equals(indi.getIndividualId())) {
-								indi2Age = indi.getAge();
-							}
+						if(arr[j].equals(indi.getIndividualId())) {
+							indi1Age = indi.getAge();
 						}
 						
-						if(indi1Age < indi2Age) {
-							String temp = arr[j];
-							arr[j] = arr[j + 1];
-							arr[j + 1] = temp;
-						};
+						if(arr[j+1].equals(indi.getIndividualId())) {
+							indi2Age = indi.getAge();
+						}
 					}
+					
+					if(indi1Age < indi2Age) {
+						String temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					};
 				}
-				
-				for(int i = 0; i < arr.length; i++) {
-					for (Iterator<Individual> iterator2 = individuals.iterator(); iterator.hasNext();) {
-						Individual indi = (Individual) iterator2.next();
-						
-						if(arr[i].equals(indi.getIndividualId())) {
-							System.out.format("%-6s%-20s%-10s%-16s%-7d%-16s%-10s", 	indi.getIndividualId(), 
+			}
+			
+			for(int i = 0; i < arr.length; i++) {
+				for (Iterator<Individual> iterator4 = individuals.iterator(); iterator4.hasNext();) {
+					Individual indi = (Individual) iterator4.next();
+					
+					if(arr[i].equals(indi.getIndividualId())) {
+						System.out.format("%-6s%-20s%-10s%-16s%-7d%-16s%-10s%-7s", 	indi.getIndividualId(), 
 																					indi.getName(),
 																					indi.getGender(),
 																					indi.getBirthDate(),
 																					indi.getAge(),
 																					indi.getAsChildOfFamily(),
-																					indi.getAsSpouseOfFamily());
-						}
+																					indi.getAsSpouseOfFamily(),
+																					fam.getFamilyId());
+						System.out.println();
+						
+						outFile.format("%-6s%-20s%-10s%-16s%-7d%-16s%-10s%-7s", 	indi.getIndividualId(), 
+																					indi.getName(),
+																					indi.getGender(),
+																					indi.getBirthDate(),
+																					indi.getAge(),
+																					indi.getAsChildOfFamily(),
+																					indi.getAsSpouseOfFamily(),
+																					fam.getFamilyId());
+						outFile.print("\r\n");
 					}
 				}
 			}
+
 		}
 		outFile.flush();
 	}
